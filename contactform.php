ecffc210206a567694c4b002daad83a40e9c6b39
Taylor Zewen <tayzewen@gmail.com>
2023-01-06 14:44:08 -0500

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,22 @@
+<?php
+
+	$myemail = 'taylor@taylorzewen.com';	
+
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$email_from = $myemail;
+	$email_subject = "New form submission";
+	$email_body = "From: $name.\n'.'Email: $visitor_email.\n'.'Message: $message.\n";
+	
+	$to = $myemail;
+
+	$headers = "From: $myemail\n";
+	$headers .= "Reply-To: $visitor_email;
+	
+	mail($to,$email_subject,$email_body,$headers)
+
+	header('Location: index.html');
+
+?> 
